c19df65fa0c499249cd5fb887b720bc42e91013b
Michał Dybich <michal.dybich@euvic.pl>
2018-01-07 20:20:58 +0100

    Implement MailService functionality

--- email-sender-server/EmailSender/Services/MailService.cs
+++ email-sender-server/EmailSender/Services/MailService.cs
@@ -1,16 +1,37 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
 using EmailSender.Models;
+using System.Net.Mail;
+using System.Net;
 
 namespace EmailSender.Services
 {
     public class MailService : IMailService
     {
+        private readonly string _hostMailAddress = "anonimowy.email.2018@gmail.com";
+
         public void SendMail(SendMailModel model)
         {
-            throw new NotImplementedException();
+            var message = new MailMessage();
+            var client = new SmtpClient();
+
+            try
+            {
+                message.From = new MailAddress(_hostMailAddress);
+                message.To.Add(model.Receiver);
+                message.Subject = model.Topic;
+                message.Body = model.Content;
+
+                client.Host = "smtp.gmail.com";
+                client.Port = 587;
+                client.Credentials = new NetworkCredential(_hostMailAddress, "Test1234%");
+                client.EnableSsl = true;
+
+                client.Send(message);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
         }
     }
 }
